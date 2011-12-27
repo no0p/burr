5ec731bcb1bb3cd4106ca648ea9ab92897e6fcc2
robert berry <berrydigital@gmail.com>
2011-12-27 15:36:54 -0800

    place_alpha implementation

--- place_alpha.m
+++ place_alpha.m
@@ -1,7 +1,7 @@
 function s = place_alpha(space, block)
   for i = 1:size(block, 1)
     space{1, i}(1, 2:3) = block(i, 1:2);
-    space{1, i}(2, 2:3) = block(i, 3:4);
+    space{1, i}(2, 2:3) = fliplr(block(i, 3:4));
   endfor
   s = space;
 endfunction
