eb7f701e4fb68307ae1df6a1e4f117d5370e881f
robert berry <berrydigital@gmail.com>
2011-12-27 16:24:28 -0800

    phi epsilon

---
+++ place_phi.m
@@ -0,0 +1,8 @@
+function s = place_phi(space, block)
+  space{1, 2}(:, 4) = block(:, 3);
+  space{1, 2}(:, 3) = block(:, 4);
+  
+  space{1, 3}(:, 4) = block(:, 2);
+  space{1, 3}(:, 3) = block(:, 1);
+  s = space;
+endfunction
