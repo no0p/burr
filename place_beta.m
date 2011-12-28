1eb84ff38555aee01f4d6d54b3f03303ca176283
robert berry <berrydigital@gmail.com>
2011-12-27 16:17:08 -0800

    place beta and gamma

---
+++ place_beta.m
@@ -0,0 +1,7 @@
+function s = place_beta(space, block)
+  for i = 1:size(block, 1)
+    space{1, i}(3, 2:3) = block(i, 1:2);
+    space{1, i}(4, 2:3) = fliplr(block(i, 3:4));
+  endfor
+  s = space;
+endfunction
